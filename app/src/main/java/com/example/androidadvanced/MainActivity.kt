cd5e33f621c2108e525a8ff355e824fa098edf63
Figt-Gonz <1781085112@qq.com>
2024-05-26 17:49:50 +0800

    feat 新增 scrollBy, Scroller, 属性动画组合等示例

--- app/src/main/java/com/example/androidadvanced/MainActivity.kt
+++ app/src/main/java/com/example/androidadvanced/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.androidadvanced
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidadvanced.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +14,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // View 动画
 //        binding.customView.animation = AnimationUtils.loadAnimation(this, R.anim.translate)
-        ObjectAnimator.ofFloat(binding.customView, "translationX", 0f, 300f)
-            .setDuration(1000)
-            .start()
+
+        // 属性动画
+//        ObjectAnimator.ofFloat(binding.customView, "alpha", 1f, 0.5f)
+//            .setDuration(1000)
+//            .start()
+//        ObjectAnimator.ofFloat(binding.customView, "translationX", 0f, 300f)
+//            .setDuration(1000)
+//            .start()
+        // 使用 Scroller 滑动视图
+//        binding.customView.smoothScrollTo(-400, -500)
+
+        // 属性动画组合
+        binding.customView.let {
+            val animator1 = ObjectAnimator.ofFloat(it,"translationX", 0.0f, 200.0f, 0f)
+            val animator2 =ObjectAnimator.ofFloat (it,"scaleX", 1.0f, 2.0f)
+            val animator3 = ObjectAnimator.ofFloat (it, "rotationX", 0.0f, 90.0f, 0.0F)
+            val animator4 = ObjectAnimator.ofFloat(it, "alpha", 1f, 0.5f)
+            AnimatorSet().apply {
+                duration = 1000
+                // animator1、animator2 同时进行，并且它们在 animator3 之后，在 animator4 之前进行
+                // 动画顺序：animator3 -> animator1 & animator2 -> animator4
+                play(animator1).with(animator2).after(animator3).before(animator4)
+                // 使这些动画同时进行
+//                playTogether(animator1, animator2, animator3, animator4)
+                start()
+            }
+        }
+
+
+
 
     }
 }
--- app/src/main/java/com/example/androidadvanced/view/CustomView.kt
+++ app/src/main/java/com/example/androidadvanced/view/CustomView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Scroller
 
 class CustomView @JvmOverloads constructor(
     context: Context,
@@ -14,6 +15,7 @@ class CustomView @JvmOverloads constructor(
 ) :
     View(context, attrs, defStyleAttr, defStyleRes) {
 
+    private val scroller = Scroller(context)
     private var lastX = 0
     private var lastY = 0
 
@@ -28,10 +30,27 @@ class CustomView @JvmOverloads constructor(
             MotionEvent.ACTION_MOVE -> {
                 val offsetX = x - lastX
                 val offsetY = y - lastY
-                moveViewByLayoutParams(offsetX, offsetY)
+                // todo 为什么下面这种 scrollTo() 效果跟 scrollBy 不一样？
+                val v = (parent as View)
+//                scrollTo(v.scrollX - offsetX, v.scrollY - offsetY)// 无法使得 View 跟随手指移动
+                v.scrollBy(-offsetX, -offsetY)// 可以使得 View 随手指移动
             }
         }
         return true
+    }
+
+    override fun computeScroll() {
+        super.computeScroll()
+        if (scroller.computeScrollOffset()) {
+            (parent as View).scrollTo(scroller.currX, scroller.currY)
+            invalidate()
+        }
+    }
+
+    fun smoothScrollTo(destX: Int, destY: Int) {
+        val deltaX = destX - scrollX
+        val deltaY = destY - scrollY
+        scroller.startScroll(scrollX, scrollY, deltaX, deltaY, 2000)
     }
 
     private fun moveViewByLayoutParams(offsetX: Int, offsetY: Int) {
